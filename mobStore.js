55fd48c8a12aeccf77dff85810548fe81b077bea
Madhusudan Mallesh <m1043036@mindtree.com>
2018-01-21 23:03:24 +0530

    iyguig

---
+++ mobStore.js
@@ -0,0 +1,124 @@
+function checkm(){
+	var x=document.getElementById("apple").checked;
+	if(x==true){
+    document.getElementById("ipn1").style.opacity = "1";
+    document.getElementById("ipn2").style.opacity = "1";
+   
+	}
+	else{
+		document.getElementById("ipn1").style.opacity = "0.1";
+		document.getElementById("ipn2").style.opacity = "0.1";
+   
+	}
+	
+	 x=document.getElementById("samsung").checked;
+	if(x==true){
+    document.getElementById("sam1").style.opacity = "1";
+    document.getElementById("sam2").style.opacity = "1";
+   
+	}
+	else{
+		document.getElementById("sam1").style.opacity = "0.1";
+    document.getElementById("sam2").style.opacity = "0.1";
+   
+	}
+	
+	 x=document.getElementById("htc").checked;
+	if(x==true){
+    document.getElementById("htc1").style.opacity = "1";
+    document.getElementById("htc2").style.opacity = "1";
+   
+	}
+	else{
+		document.getElementById("htc1").style.opacity = "0.1";
+    document.getElementById("htc2").style.opacity = "0.1";
+   
+	}
+	
+	 x=document.getElementById("nokia").checked;
+	if(x==true){
+    document.getElementById("nokia1").style.opacity = "1";
+    document.getElementById("nokia2").style.opacity = "1";
+   
+	}
+	else{
+		document.getElementById("nokia1").style.opacity = "0.1";
+    document.getElementById("nokia2").style.opacity = "0.1";
+   
+	}
+	
+	 x=document.getElementById("sony").checked;
+	if(x==true){
+    document.getElementById("sony1").style.opacity = "1";
+    document.getElementById("sony2").style.opacity = "1";
+   
+	}
+	else{
+		document.getElementById("sony1").style.opacity = "0.1";
+    document.getElementById("sony2").style.opacity = "0.1";
+   
+	}
+   
+}
+function checks(){
+	var x=document.getElementById("apple").checked;
+	if(x==true){
+    document.getElementById("ipn1").style.opacity = "1";
+    document.getElementById("ipn2").style.opacity = "1";
+   
+	}
+	else{
+		document.getElementById("ipn1").style.opacity = "0.1";
+    document.getElementById("ipn2").style.opacity = "0.1";
+   
+	}
+	
+	 x=document.getElementById("samsung").checked;
+	if(x==true){
+    document.getElementById("sam1").style.opacity = "1";
+    document.getElementById("sam2").style.opacity = "1";
+   
+	}
+	else{
+		document.getElementById("sam1").style.opacity = "0.1";
+    document.getElementById("sam2").style.opacity = "0.1";
+   
+	}
+	
+	 x=document.getElementById("htc").checked;
+	if(x==true){
+    document.getElementById("htc1").style.opacity = "1";
+    document.getElementById("htc2").style.opacity = "1";
+   
+	}
+	else{
+		document.getElementById("htc1").style.opacity = "0.1";
+    document.getElementById("htc2").style.opacity = "0.1";
+   
+	}
+	
+	 x=document.getElementById("nokia").checked;
+	if(x==true){
+    document.getElementById("nokia1").style.opacity = "1";
+    document.getElementById("nokia2").style.opacity = "1";
+   
+	}
+	else{
+		document.getElementById("nokia1").style.opacity = "0.1";
+    document.getElementById("nokia2").style.opacity = "0.1";
+   
+	}
+	
+	 x=document.getElementById("sony").checked;
+	if(x==true){
+    document.getElementById("sony1").style.opacity = "1";
+    document.getElementById("sony2").style.opacity = "1";
+   
+	}
+	else{
+		document.getElementById("sony1").style.opacity = "0.1";
+    document.getElementById("sony2").style.opacity = "0.1";
+   
+	}
+   
+}
